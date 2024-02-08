d0a253b1974868faa649ddd77c141168c3121982
George Davis <davisgl055@ecc.edu>
2024-02-07 19:20:29 -0500

    Seemed to finish function header

--- rationalNumber.h
+++ rationalNumber.h
@@ -6,18 +6,29 @@ class rationalNumber
 {
 	public:
 		void setRationalNumber(); // Gets the rational number from the user
-		void operatorgetRationalNumber(); // Accessor function
+		void getRationalNumber(); // Accessor function
 		double operator+(rationalNumber // Right hand side value 
 			); // Addition operator
 		double operator-(rationalNumber // Right hand side value
 		); // Subtraction operator
 		double operator*(rationalNumber // Right hand side value
 		); // Multiplication operator
-		double division(rationalNumber // Right hand side value
+		double operator/(rationalNumber // Right hand side value
 		); // Division operator
-
-	
-
+		double operator==(rationalNumber // Right hand side value
+		); // Equlaity Operator
+		double operator<=(rationalNumber // Right hand side value
+		); // Less than or equal to operator
+		double operator>=(rationalNumber // Right hand side value
+		); // Greater than or equal to operator
+		double operator<(rationalNumber // Right hand side value
+		); // Less than oeprator
+		double operator>(rationalNumber // Right hand side value
+		); // Greater than operator
+		ostream& operator<<(rationalNumber // Right hand side value
+		); // Stream insertion operator
+		istream& operator>>(rationalNumber // Right hand side value
+		); // Stream extraction operator
 	private:
 		string rationalNumber; // This is the rational number
 };
