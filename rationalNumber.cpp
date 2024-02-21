d69a7b40b390981112ff90bf2d1b0f3f211da44c
George Davis <davisgl055@ecc.edu>
2024-02-21 00:57:25 -0500

    Made more functions and made function to simplify fractions operator+ isn't working.

--- rationalNumber.cpp
+++ rationalNumber.cpp
@@ -1,4 +1,4 @@
-#include "rationalNumber.h"
+#include "RationalNumber.h"
 
 void RationalNumber::setRationalNumber(istream& in) 
 {
@@ -22,7 +22,41 @@ bool RationalNumber::checkValidRationalNumber(string& num)
 	}
 	return true;
 }
+void RationalNumber::simplifyRationalNumber()
+{
+	// Factor the fraction
+	while (numerator % demoniator != 0)
+	{
+		demoniator = numerator % demoniator; // Sets demoniator to the remainder of the division
+		numerator = demoniator; // Sets numerator to the divisor
+	}
+	// Simplify the fraction
+	numerator /= demoniator;
+	demoniator /= demoniator;
+}
 void RationalNumber::outputRationalNumber(ostream& out) 
 {
 	out << " " << numerator << "/" << demoniator;
 }
+int RationalNumber::getNum() 
+{
+	return numerator;
+}
+void RationalNumber::setNum(int value) 
+{
+	numerator = value;
+}
+int RationalNumber::getDen() 
+{
+	return demoniator;
+}
+void RationalNumber::setDen(int value) 
+{
+	demoniator = value;
+}
+
+double operator+(RationalNumber& num2)
+{
+	RationalNumber output;
+}
+
--- rationalNumber.h
+++ rationalNumber.h
@@ -7,36 +7,37 @@ class RationalNumber
 	public:
 		void setRationalNumber(istream&); // Gets the rational number from the user
 		bool checkValidRationalNumber(string&); // Checks if a rational number is valid
+		void simplifyRationalNumber(); // Simplifies the rational number
 		void outputRationalNumber(ostream&); // Outputs rational number function
-		void getNum();
-		void setNum();
-		void getDen();
-		void setDen();
-		int numerator, demoniator; // These numbers represent the top & bottom part of the fraction
-		double operator+(RationalNumber // Right hand side value 
+		int getNum(); // Gets Numerator
+		void setNum(int); // Sets Numerator
+		int getDen(); // Gets Denomiator
+		void setDen(int); // Sets Denomiator
+		friend double operator+(RationalNumber& // Right hand side value 
 			); // Addition operator
-		double operator-(RationalNumber // Right hand side value
+		double operator-(RationalNumber& // Right hand side value
 		); // Subtraction operator
-		double operator*(RationalNumber // Right hand side value
+		double operator*(RationalNumber& // Right hand side value
 		); // Multiplication operator
-		double operator/(RationalNumber // Right hand side value
+		double operator/(RationalNumber& // Right hand side value
 		); // Division operator
-		double operator==(RationalNumber // Right hand side value
+		double operator==(RationalNumber& // Right hand side value
 		); // Equlaity Operator
-		double operator<=(RationalNumber // Right hand side value
+		double operator<=(RationalNumber& // Right hand side value
 		); // Less than or equal to operator
-		double operator>=(RationalNumber // Right hand side value
+		double operator>=(RationalNumber& // Right hand side value
 		); // Greater than or equal to operator
-		double operator<(RationalNumber // Right hand side value
+		double operator<(RationalNumber& // Right hand side value
 		); // Less than oeprator
-		double operator>(RationalNumber // Right hand side value
+		double operator>(RationalNumber& // Right hand side value
 		); // Greater than operator
-		ostream& operator<<(RationalNumber // Right hand side value
+		ostream& operator<<(RationalNumber& // Right hand side value
 		); // Stream insertion operator
-		istream& operator>>(RationalNumber // Right hand side value
+		istream& operator>>(RationalNumber& // Right hand side value
 		); // Stream extraction operator
 	private:
 		string RationalNumber; // This is the rational number
+		int numerator = 0, demoniator = 1; // These numbers represent the top & bottom part of the fraction
 };
 #endif
 
